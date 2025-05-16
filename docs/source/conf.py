fe17f31a30f4b2425972982180171b63692d9b9b
Quant Girl <quantgirl.uk@gmail.com>
2025-05-16 21:33:04 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,6 +62,7 @@ html_static_path = ['_static']
 
 
 sphinx_gallery_conf = {
+    'image_scrapers': ('matplotlib',),  # Use matplotlib directly (if you have plots)
     'within_subsection_order': "FileNameSortKey",
     'examples_dirs': ["../../examples"] ,  # path to your example scripts
     'gallery_dirs': ['auto_examples'],  # path to where to save gallery generated output

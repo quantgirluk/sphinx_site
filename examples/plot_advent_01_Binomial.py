f12887cf6378acdfd501a2161e419376b061c7e9
Quant Girl <quantgirl.uk@gmail.com>
2025-05-16 22:48:34 +0100

    Update plot_advent_01_Binomial.py

--- examples/plot_advent_01_Binomial.py
+++ examples/plot_advent_01_Binomial.py
@@ -23,8 +23,8 @@ n = 20
 p = 0.3
 rv = binom(n=n, p=p)
 x = np.arange(0, n + 1, 1)
-title = f"\n Binomial Distribution \n $X \sim Bin(n={n}, p={p}$)"
-
+# title = f"\n Binomial Distribution \n $X \sim Bin(n={n}, p={p}$)"
+title = "Binomial"
 fig = plt.figure(figsize=(10, 5), dpi=150)
 gs = GridSpec(1, 4, wspace=0.5)
 ax1 = fig.add_subplot(gs[:2])

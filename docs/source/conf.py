a809e6eee507b70729f1f6e8ce8780e760ee1d33
Quant Girl <quantgirl.uk@gmail.com>
2025-05-16 21:22:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,17 @@ author = 'D.Santiago'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+
+extensions = ['sphinx_gallery.gen_gallery',
+              'myst_parser',
+              'sphinx.ext.mathjax',
+              ]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,3 +59,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+sphinx_gallery_conf = {
+    'within_subsection_order': "FileNameSortKey",
+    'examples_dirs': ["../../examples"] ,  # path to your example scripts
+    'gallery_dirs': ['auto_examples'],  # path to where to save gallery generated output
+}

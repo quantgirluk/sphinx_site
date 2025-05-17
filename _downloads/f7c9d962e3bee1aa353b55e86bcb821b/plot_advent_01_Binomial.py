bb2224836d7e79a9f14b5c1860834fdc36f3bb7d
quantgirluk <quantgirluk@users.noreply.github.com>
2025-05-17 16:12:19 +0000

    deploy: c5aadfbe62cda64ae739072113b28f096b7ee17b

---
+++ _downloads/f7c9d962e3bee1aa353b55e86bcb821b/plot_advent_01_Binomial.py
@@ -0,0 +1,50 @@
+"""
+Day 1 : Binomial
+=========================
+
+"""
+
+###############################################################################
+
+# Author: Dialid Santiago <d.santiago@outlook.com>
+# License: MIT
+# Description: Advent Calendar Day 1
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+from scipy.stats import binom
+import numpy as np
+
+# import matplotlib as mpl
+# mpl.rcParams.update(mpl.rcParamsDefault)
+#
+plt.style.use(
+    "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle")
+
+n = 20
+p = 0.3
+rv = binom(n=n, p=p)
+x = np.arange(0, n + 1, 1)
+title = f"\n Binomial Distribution \n $X \sim Bin(n={n}, p={p}$)"
+# title = "Binomial"
+fig = plt.figure(figsize=(10, 5), dpi=150)
+gs = GridSpec(1, 4, wspace=0.5)
+ax1 = fig.add_subplot(gs[:2])
+ax2 = fig.add_subplot(gs[2:])
+
+ax1.vlines(x, 0, rv.pmf(x), colors='grey', linestyles='--', lw=1)
+ax1.plot(x, rv.pmf(x), marker='o', linestyle='', color='deepskyblue')
+
+plt.plot(x, rv.cdf(x), drawstyle='steps-post',
+         color='grey', lw=1, linestyle='--')
+ax2.plot(x, rv.cdf(x), color='hotpink', marker='o', linestyle='')
+
+ax1.set_title('Probability Mass Function', y=-0.18)
+ax2.set_title('Cumulative Distribution Function', y=-0.18)
+
+fig.suptitle(title)
+gs.tight_layout(fig)
+
+# fig.savefig('01_Binomial')
+plt.show()
---
+++ _sources/index.rst
@@ -0,0 +1,32 @@
+.. sphinx_test documentation master file, created by
+   sphinx-quickstart on Fri May 16 20:45:07 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sphinx_test's documentation!
+=======================================
+
+Hello
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+   :caption: Galleries and Source Code
+
+   auto_examples/index
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+.. raw:: html
+
+   <div style="text-align: center;">
+       Thanks for visiting ✨
+   </div>
